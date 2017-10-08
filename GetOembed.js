6a0436618484bae0315b9f4319037812aa521b61
Steve Zhang <zhming@microsoft.com>
2017-10-08 20:51:10 +0800

    aaync done

--- GetOembed.js
+++ GetOembed.js
@@ -1,11 +1,10 @@
-var http = require('http');
+var https = require('https');
 var async = require('async');
+const {
+    URL
+} = require('url');
 
-var options = {
-    host: 'oembed.com',
-    port: 80,
-    path: '/'
-};
+var options = new URL('https://oembed.com/');
 
 var providerStrArray = [];
 var providerArray = [];
@@ -25,10 +24,9 @@ async.series({
     },
     three: function (done) {
         var newAsync = require('async');
-        newAsync.parallelLimit(providerchecking
-            , 2, function (error, result) {
-                done(null, null);
-            });
+        newAsync.parallelLimit(providerchecking, 100, function (error, result) {
+            done(null, null);
+        });
     }
 }, function (error, result) {
     console.log('end');
@@ -39,19 +37,20 @@ async.series({
 
 function getoEmbedProviders(done) {
 
-    http.get(options, function (res) {
+    https.get(options, function (res) {
 
         //var contentType = res.headers['content-type'];
 
         let error;
 
         if (res.statusCode != 200) {
-            error = new Error('Request to get oEmbed.com Homepage failed\n' + `Status Code: ${statuscode}`);
+            error = new Error('Request to get oEmbed.com Homepage failed\n' + `Status Code: ${res.statusCode}`);
         }
 
         if (error) {
             console.error(error.message);
             res.resume();
+            return;
         }
 
         var rawdata = '';
@@ -85,71 +84,136 @@ var checkoEmbdProvider = function (provider) {
     };
 };
 
-function checkProvider(provider, done) {
+function checkProvider(provider, CheckProviderDone) {
 
-    var checkexamples = [];
+    var checkexampleArray = [];
+    var exampleArray = [];
     for (var item in provider.examples) {
-        var example;
+        var example = {
+            url: '',
+            htmlcode: '',
+            stat: 'needcheck'
+        };
         example.url = provider.examples[item];
-        example.stat = 'needcheck';
-        checkexamples.push(getexample(example.url, example));
+        exampleArray.push(example);
+        checkexampleArray.push(getexample(example.url, example));
     }
 
-    var checkasync = new('async');
 
-        /*
-        httplocal.get(option, function (res) {
-            res.on('data', function (data) {
-                embedcode = embedcode + data;
-            }).on('end', function () {
-                var response = "";
-                embedcode = embedcode.trim();
-                var starsig = '^{';
-                var startregxx = new RegExp('^{');
-                var ifxml = embedcode.match('<?xml') == null ? false : true;
-                var ifjson = embedcode.match(startregxx) == null ? false : true;
-                if (!ifxml || !ifjson) {
-                    provider.working = false;
-                } else {
+    var newsync = require('async');
+    newsync.series({
+            one: function (done) {
+                var checkasync = require('async');
+                checkasync.parallelLimit(checkexampleArray, 5, function (error, result) {
+                    done(null, null);
+                });
+            },
+            two: function (done) {
+                for (var i in provider.examples) {
+                    console.log('#' + i + ' ' + exampleArray[i].url + '\n');
+                    console.log(exampleArray[i].stat + '\n');
 
-                    var json = embedcode;
-                    if (ifxml) {
-                        var parsestring = require('xml2js').parseString;
-                        parserstring(embedcode, function (err, result) {
-                            json = result;
-                        });
-                    }
                 }
-            }).on('error', function(data){
-                console.log('error in', provider.name);
-            });
-        });*/
+                done(null, null);
+            }
 
-    done(null, null);
+        }, function (errer, result) {
+            CheckProviderDone(null, null);
+        }
+
+    );
+
+    /*
+    httplocal.get(option, function (res) {
+        res.on('data', function (data) {
+            embedcode = embedcode + data;
+        }).on('end', function () {
+            var response = "";
+            embedcode = embedcode.trim();
+            var starsig = '^{';
+            var startregxx = new RegExp('^{');
+            var ifxml = embedcode.match('<?xml') == null ? false : true;
+            var ifjson = embedcode.match(startregxx) == null ? false : true;
+            if (!ifxml || !ifjson) {
+                provider.working = false;
+            } else {
+
+                var json = embedcode;
+                if (ifxml) {
+                    var parsestring = require('xml2js').parseString;
+                    parserstring(embedcode, function (err, result) {
+                        json = result;
+                    });
+                }
+            }
+        }).on('error', function(data){
+            console.log('error in', provider.name);
+        });
+    });*/
+
 }
 
-function getexample (option, ret){
-    return function (syncdone){
+function getexample(option, ret) {
+    return function (syncdone) {
         return getHttp(option, ret, syncdone);
-    }
+    };
 }
 
-function getHttp(option, ret, syncdone){
+function getHttp(option, ret, getHttpDone) {
+    var httpsClientRequester = require('https');
     var httpClientRequester = require('http');
     var htmlcode = '';
-    httpClientRequester.get(url, function (res){
-        res.on('data', function(data){
-            htmlcode = htmlcode + data;
-        }).on('end', function(){
-            ret.htmlcode = htmlcode;
-            ret.stat = 'success';
-            syncdone(null, null);
-        }).on('error', function(data){
-            ret.htmlcode = htmlcode;
-            ret.stat = 'fail';
-            syncdone(null, null);
-        });
+
+    const d = require('domain').create();
+    d.on('error', function (err) {
+        console.log(err.message);
+        getHttpDone(null, null);
     });
+    console.log('get example from:' + option);
+
+    if (option.match('^https://') != null)
+        d.add(httpsClientRequester.get(option, function (res) {
+            res.on('data', function (data) {
+                htmlcode = htmlcode + data;
+            }).on('end', function () {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'success';
+                getHttpDone(null, null);
+            }).on('error', function (data) {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'fail';
+                getHttpDone(null, null);
+            });
+        }));
+    else if (option.match('^http://') == null) {
+        option = 'https://' + option;
+        d.add(httpsClientRequester.get(option, function (res) {
+            res.on('data', function (data) {
+                htmlcode = htmlcode + data;
+            }).on('end', function () {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'success';
+                getHttpDone(null, null);
+            }).on('error', function (data) {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'fail';
+                getHttpDone(null, null);
+            });
+        }));
+    } else
+        d.add(httpClientRequester.get(option, function (res) {
+            res.on('data', function (data) {
+                htmlcode = htmlcode + data;
+            }).on('end', function () {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'success';
+                getHttpDone(null, null);
+            }).on('error', function (data) {
+                ret.htmlcode = htmlcode;
+                ret.stat = 'fail';
+                getHttpDone(null, null);
+            });
+        }));
 }
 /* function getOption(url) {
 
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-var isType = function(type){
-  return function(obj){
-      return toString.call(obj) == '[object '+type+']';
-    }
-  };
-  var isArray = isType('Array');
-  if(isArray(11)){
-      console.log('this is array');
-  }else
-    console.log('this is not array');
+
+  var async = require('async');
+  // run 'my_task' 100 times, with parallel limit of 10
+
+  var my_task = function(callback) { callback(null, 'one');console.log('test') };
+  var when_done = function(err, results) { console.log('done')};
+
+  // create an array of tasks
+  var async_queue = Array(100).fill(my_task);
+
+  async.parallelLimit(async_queue, 1, when_done);
